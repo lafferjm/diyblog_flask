79f53531206a860d274d24bf21ebb634f8f68065
Justin Lafferty <lafferjm@gmail.com>
2019-05-01 13:23:44 -0500

    register flask migrate

--- diyblog/__init__.py
+++ diyblog/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from diyblog.config import Config
 
 db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app(test_config=None):
     app = Flask(__name__)
@@ -13,6 +15,7 @@ def create_app(test_config=None):
         app.config.from_mapping(test_config)
 
     db.init_app(app)
+    migrate.init_app(app, db)
 
     from . import blog
     app.register_blueprint(blog.bp)
