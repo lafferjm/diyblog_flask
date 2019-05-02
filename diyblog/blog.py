236bf17528fb52dff870e42db36c9e3f0989631a
Justin Lafferty <lafferjm@gmail.com>
2019-05-02 07:55:40 -0500

    hardcoding stats until admin interface

--- diyblog/blog.py
+++ diyblog/blog.py
@@ -1,7 +1,17 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 bp = Blueprint('blog', __name__, url_prefix='/blog')
 
 @bp.route('/')
 def index():
-    return 'Blog Index'
+    num_authors = 0
+    num_posts = 0
+    num_comments = 0
+
+    stats = {
+        'num_authors': num_authors,
+        'num_posts': num_posts,
+        'num_comments': num_comments
+    }
+
+    return render_template('blog/index.html', stats=stats)
